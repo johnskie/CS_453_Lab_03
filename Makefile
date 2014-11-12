85ad81291bf509e6c202910611b2a4f33d41c981
dupontta <dupontta@uwec.edu>
2014-11-12 11:01:41 -0600

    Error in main with strcmp.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # http://www.haverford.edu/cmsc/course-pages/usingMake.html
 
-all: mfqs.o clock.o process.o
-	g++ -g process.o clock.o mfqs.o -lm -o myprogram
+all: main.o mfqs.o clock.o process.o
+	g++ process.o clock.o mfqs.o main.o -lm -o myprogram
 process.o : Process.cpp Process.h
 	g++ -c Process.cpp -o process.o
 
@@ -11,4 +11,5 @@ clock.o : Clock.cpp Clock.h
 mfqs.o : mfqs.cpp mfqs.h
 	g++ -c mfqs.cpp -o mfqs.o
 
-
+main.o : main.cpp
+	g++ -c main.cpp
--- main.cpp
+++ main.cpp
@@ -13,33 +13,33 @@ void inputParser(string s, int results[])
     int pos = 0;
     for (int i = 0; i <= s.length(); i++)
     {
-        if(s[i] == '\t')
+        if (s[i] == '\t')
         {
-            const char* ss = s.substr(pos, i - pos).c_str();
+            const char *ss = s.substr(pos, i - pos).c_str();
             cnt++;
             pos = i + 1;
         }
         else if (i == s.length())
         {
-            const char * ss = s.substr(pos, i - pos).c_str();
+            const char *ss = s.substr(pos, i - pos).c_str();
             results[cnt] = atoi(ss);
         }
     }
 }
 
 
-int main(int argc, char* args[])
+int main(int argc, char *args[])
 {
     bool valid = false, valid2 = false;
     string in = "", nQueues;
-    int numQueues, at, quantum;
+    int numQueues, age, quantum;
 
     cout << "Which scheduling algorithm would you like to use? (m = mfqs)" << endl;
     while (!valid)
     {
         getline(cin, in);
 
-        if((in.compare("m") == 0))
+        if (strcmp(in, "m") == 0)
         {
             while (!valid2)
             {
@@ -61,9 +61,43 @@ int main(int argc, char* args[])
             while (!valid2)
             {
                 cout << "Enter time for ageing: " << endl;
-
+                cin >> age;
+                if (age > 0)
+                {
+                    valid2 = true;
+                }
 
             }
+
+            valid2 = false;
+            while (!valid2)
+            {
+                cout << "Enter quantum: " << endl;
+                cin >> quantum;
+
+                if (quantum > 0)
+                {
+                    valid2 = true;
+                    valid = true;
+                }
+            }
+        }
+        else if (strcmp(in, "r") == 0)
+        {
+            // do RTS stuff
+            valid = true;
+        }
+        else if (strcmp(in, "wh"))
+        {
+            // do windows hybrid
+            valid = true;
+        }
+        else
+        {
+            cout << "Invalid input. Please select: m = MFQS, r = RTS, wh = Windows Hybrid" << endl;
         }
     }
+
+
+    return 0;
 }
