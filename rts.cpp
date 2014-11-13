a7679f341f8e3ecdb6d069e6acd6deeb6953ef19
JOHNSKIE <johnskie@dplsubmit.cs.uwec.edu>
2014-11-12 21:54:37 -0600

    rts work in progress

--- rts.cpp
+++ rts.cpp
@@ -4,17 +4,19 @@ using namespace std;
 
 RTS::RTS(int numQueues, int at, int quantum, Process *processArray)
 {
-	this->numQueues = numQueues;
 	this->at = at;
 	this->quantum = quantum;
 	this->processArray = processArray;
 	totalwait = 0;
 	totalTurnaround = 0;
-	queueList[numQueues - 1];
 	c = Clock();
 	currentP = processArray;
 }
-
+// as far as i can remember rts cares aboutthe arrival time, the burst time(time quantum), and the end time
+// once a process starts (sort by arrival time, then by priority and if that still doesnt do it, by PID)
+// if the process finishes before it's alloted time quantum, insert the next process until the end of burst, then check for the next process to go (decrement runtime of inserted process as needed)
+// make sure processes run by deadlines, if they cannot report them as failed (PID) and remove from queue
+// ignore IO same as mfqs
 void RTS::runProcess(Process *currentProcess, int q1, int curQuantum, int currentRemaining)
 {
 	if (q1 == 0)
