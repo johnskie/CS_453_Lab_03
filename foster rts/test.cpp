cd15552f6febc569962a5851e5ac71a596647bef
johnskie <johnskie@uwec.edu>
2015-11-24 03:24:46 -0600

    added all rts stuff, some of it is just scratch and tinkering, but the main seems to work

---
+++ foster rts/test.cpp
@@ -0,0 +1,21 @@
+
+
+read in all processes to schedule
+allProc = []
+
+start clock
+check if allProc[newindex].arrival < clock
+
+add to priority queue to tick
+pq = allproc[newindex]
+newindex++
+
+check if proc[newindex] < clock
+
+while( pq ! empty){
+	process pq
+	tick clock
+	check if 
+	check if proc[newindex] < clock
+}
+
